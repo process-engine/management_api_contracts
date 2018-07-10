8dc8f93976f3c0ee30cd9f5ac3e0b0eed4df89d7
5ebastianMeier <sebastian.meier@5minds.de>
2018-07-10 13:18:47 +0200

    add process model execution namespace to index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import * as processModelExecution from './process_model_execution/index';
-
-export const ProcessModelExecution = processModelExecution;
+// tslint:disable:no-namespace
 
 export * from './management_context';
 export * from './management_request';
+export * from './imanagement_api_service';
+export * from './rest_settings';
+
+import * as execution from './process_model_execution/index';
+
+export namespace ProcessModelExecution {
+  export import ProcessStartRequestPayload = execution.ProcessStartRequestPayload;
+  export import ProcessStartResponsePayload = execution.ProcessStartResponsePayload;
+  export import StartCallbackType = execution.StartCallbackType;
+}
