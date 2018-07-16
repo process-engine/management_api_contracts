fde4464c5f76895464f3c63f0b00de7a32d64b4f
5ebastianMeier <sebastian.meier@5minds.de>
2018-07-16 13:19:29 +0200

    fix unintended mandatory correlation id

--- src/process_model_execution/process_start_request_payload.ts
+++ src/process_model_execution/process_start_request_payload.ts
@@ -1,5 +1,5 @@
 export class ProcessStartRequestPayload {
-  public correlationId: string;
+  public correlationId?: string;
    // Note: The CallerId contains a process instance ID and must only ever be set, when a subprocess is to be started.
   public callerId?: string;
   public inputValues: any;
