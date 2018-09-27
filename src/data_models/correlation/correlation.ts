7f0f62414141c9b064a105d3f0e29ec0a4ad9a8d
ElRaptorus <christian.werner@5minds.de>
2018-09-27 10:23:12 +0200

    :sparkles: Add identity to correlation object

--- src/data_models/correlation/correlation.ts
+++ src/data_models/correlation/correlation.ts
@@ -1,3 +1,5 @@
+import {IIdentity} from '@essential-projects/iam_contracts';
+
 import {CorrelationProcessModel} from './correlation_process_model';
 import {CorrelationState} from './correlation_state';
 
@@ -8,5 +10,6 @@ export class Correlation {
   public id: string;
   public processModels: Array<CorrelationProcessModel>;
   public state: CorrelationState;
+  public identity: IIdentity;
   public createdAt?: Date;
 }
