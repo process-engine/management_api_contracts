a85be71f8979cd2e139a7300f8d863f5871c85c9
Alex😎 <alexander.kasten@5minds.de>
2018-11-12 16:45:39 +0100

    :sparkles: Add ManualTask callback types

--- src/data_models/messages/callback_types.ts
+++ src/data_models/messages/callback_types.ts
@@ -1,7 +1,17 @@
-import {ProcessEndedMessage, ProcessTerminatedMessage, UserTaskFinishedMessage, UserTaskReachedMessage} from './system_events';
+import {
+  ManualTaskFinishedMessage,
+  ManualTaskReachedMessage,
+  ProcessEndedMessage,
+  ProcessTerminatedMessage,
+  UserTaskFinishedMessage,
+  UserTaskReachedMessage,
+} from './system_events';
 
 export type OnUserTaskWaitingCallback = (userTaskWaiting: UserTaskReachedMessage) => void|Promise<void>;
 export type OnUserTaskFinishedCallback = (userTaskFinished: UserTaskFinishedMessage) => void|Promise<void>;
+
+export type OnManualTaskWaitingCallback = (manualTaskWaiting: ManualTaskReachedMessage) => void|Promise<void>;
+export type OnManualTaskFinishedCallback = (manualTaskFinished: ManualTaskFinishedMessage) => void|Promise<void>;
 
 export type OnProcessEndedCallback = (processEnded: ProcessEndedMessage) => void|Promise<void>;
 export type OnProcessTerminatedCallback = (processTerminated: ProcessTerminatedMessage) => void|Promise<void>;
