0ecfa916785b36ae4556db5c6ab75ba4ffa7f745
Alex😎 <alexander.kasten@5minds.de>
2018-11-16 09:16:16 +0100

    :fire: Remove unnecessary comments @cmg-dev

--- src/data_models/manual_task/manual_task.ts
+++ src/data_models/manual_task/manual_task.ts
@@ -2,28 +2,10 @@
  * Describes a maual task that belongs to an active correlation.
  */
 export class ManualTask {
-  /**
-   * The ID of the ManualTask.
-   */
   public id: string;
-  /**
-   * The name of the ManualTask.
-   */
   public name: string;
-  /**
-   * The ID of the correlation that the ManualTask belongs to.
-   */
   public correlationId: string;
-  /**
-   * The ID of the process model that the ManualTask belongs to.
-   */
   public processModelId: string;
-  /**
-   * The ID of the process instance that the ManualTask belongs to.
-   */
   public processInstanceId?: string;
-  /**
-   * The token payload the ManualTask got suspended with.
-   */
   public tokenPayload: any;
 }
--- src/data_models/manual_task/manual_task_list.ts
+++ src/data_models/manual_task/manual_task_list.ts
@@ -1,11 +1,5 @@
 import {ManualTask} from './manual_task';
 
-/**
- * Describes a list of manual tasks.
- */
 export class ManualTaskList {
-  /**
-   * The manual task list.
-   */
   public manualTasks: Array<ManualTask> = [];
 }
