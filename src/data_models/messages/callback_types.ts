e478c47fbaaf43cd044b439e00f7f53a305f9e87
Alex <alexanderkasten@users.noreply.github.com>
2018-11-16 09:10:11 +0100

    :lipstick: Add Whitespaces
    Co-Authored-By: alexanderkasten <alexanderkasten@users.noreply.github.com>

--- src/data_models/messages/callback_types.ts
+++ src/data_models/messages/callback_types.ts
@@ -7,11 +7,11 @@ import {
   UserTaskReachedMessage,
 } from './system_events';
 
-export type OnUserTaskWaitingCallback = (userTaskWaiting: UserTaskReachedMessage) => void|Promise<void>;
-export type OnUserTaskFinishedCallback = (userTaskFinished: UserTaskFinishedMessage) => void|Promise<void>;
+export type OnUserTaskWaitingCallback = (userTaskWaiting: UserTaskReachedMessage) => void | Promise<void>;
+export type OnUserTaskFinishedCallback = (userTaskFinished: UserTaskFinishedMessage) => void | Promise<void>;
 
-export type OnManualTaskWaitingCallback = (manualTaskWaiting: ManualTaskReachedMessage) => void|Promise<void>;
-export type OnManualTaskFinishedCallback = (manualTaskFinished: ManualTaskFinishedMessage) => void|Promise<void>;
+export type OnManualTaskWaitingCallback = (manualTaskWaiting: ManualTaskReachedMessage) => void | Promise<void>;
+export type OnManualTaskFinishedCallback = (manualTaskFinished: ManualTaskFinishedMessage) => void | Promise<void>;
 
-export type OnProcessEndedCallback = (processEnded: ProcessEndedMessage) => void|Promise<void>;
-export type OnProcessTerminatedCallback = (processTerminated: ProcessTerminatedMessage) => void|Promise<void>;
+export type OnProcessEndedCallback = (processEnded: ProcessEndedMessage) => void | Promise<void>;
+export type OnProcessTerminatedCallback = (processTerminated: ProcessTerminatedMessage) => void | Promise<void>;
