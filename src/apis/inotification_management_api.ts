0d93a4c6f87c25512516bcb75cdbba41d48125a6
ElRaptorus <christian.werner@5minds.de>
2019-01-21 11:58:58 +0100

    :fire: Remove unsubscribeAllForIdentity UseCase This isn't yet viable because the EventAggregator is missing functionality to group subscriptions.

--- src/apis/inotification_management_api.ts
+++ src/apis/inotification_management_api.ts
@@ -1,5 +1,4 @@
 import {Subscription} from '@essential-projects/event_aggregator_contracts';
-import {IIdentity} from '@essential-projects/iam_contracts';
 
 /**
  * The INotificationManagementApi is to manage subscriptions for async notifications.
@@ -13,12 +12,4 @@ export interface INotificationManagementApi {
    * @param subscription The subscription to remove.
    */
   unsubscribe(subscription: Subscription): Promise<void>;
-
-  /**
-   * Removes all notification subscriptions for the given identity.
-   *
-   * @async
-   * @param identity The identity for which to remove all subscriptions.
-   */
-  unsubscribeAllForIdentity(identity: IIdentity): Promise<void>;
 }
