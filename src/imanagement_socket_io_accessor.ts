8d3ef6d175d43605bfe14fb703433e43a3258f21
ElRaptorus <christian.werner@5minds.de>
2019-01-22 11:38:14 +0100

    :sparkles: Provide an interface for a Socket.IO client

---
+++ src/imanagement_socket_io_accessor.ts
@@ -0,0 +1,22 @@
+import {IIdentity} from '@essential-projects/iam_contracts';
+
+/**
+ * Provides functions for managing Socket.IO clients.
+ */
+export interface IManagementSocketIoAccessor {
+
+  /**
+   * Uses the given Identity to connect this client to the Socket.IO server.
+   *
+   *
+   * @param identity The identity with which to create the connection.
+   */
+  initializeSocket(identity: IIdentity): void;
+
+  /**
+   * Uses the given Identity to disconnect this client from the Socket.IO server.
+   *
+   * @param identity The identity with which to create the connection.
+   */
+  disconnectSocket(identity: IIdentity): void;
+}
