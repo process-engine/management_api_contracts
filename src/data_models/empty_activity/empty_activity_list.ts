beefc01cbb83d94f66557e3cb3c5df9006303d0a
ElRaptorus <christian.werner@5minds.de>
2019-02-28 15:03:08 +0100

    :sparkles: Add data types for EmptyActivities

---
+++ src/data_models/empty_activity/empty_activity_list.ts
@@ -0,0 +1,8 @@
+import {EmptyActivity} from './empty_activity';
+
+/**
+ * Describes a list of EmptyActivities.
+ */
+export class EmptyActivityList {
+  public emptyActivities: Array<EmptyActivity> = [];
+}
