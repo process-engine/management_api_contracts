1bc76052c92f725bd3c5e555d41a1ac3a05f7a9b
Steffen <Steffen.knaup@5minds.de>
2019-03-25 10:00:57 +0100

    :sparkles: Add Functions to Get Logs by ProcessInstanceId

--- src/apis/ilogging_management_api.ts
+++ src/apis/ilogging_management_api.ts
@@ -21,4 +21,19 @@ export interface ILoggingManagementApi {
    *                             valid auth token.
    */
   getProcessModelLog(identity: IIdentity, processModelId: string, correlationId?: string): Promise<Array<LogEntry>>;
+
+  /**
+   * Retrieves the logs for a specific ProcessInstance.
+   *
+   * @async
+   * @param   identity           The requesting users identity.
+   * @param   processModelId     The ID of the ProcessModel for which to retrieve the
+   *                             logs.
+   * @param   processInstance    The ID of the ProcessInstance for which to retrieve the
+   *                             logs.
+   * @returns                    A list of log entries.
+   * @throws {UnauthorizedError} If the given identity does not contain a
+   *                             valid auth token.
+   */
+  getProcessInstanceLog(identity: IIdentity, processModelId: string, processInstanceId: string): Promise<Array<LogEntry>>;
 }
